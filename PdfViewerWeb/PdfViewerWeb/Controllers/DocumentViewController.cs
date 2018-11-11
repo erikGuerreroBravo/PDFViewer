1c100129bbab12d7b7b9f5eebb40620490d91423
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2018-11-10 18:12:53 -0600

    Subiendo una alternativa a la presentacion del proyecto

---
+++ PdfViewerWeb/PdfViewerWeb/Controllers/DocumentViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PdfViewerWeb.Controllers
+{
+    public class DocumentViewController : Controller
+    {
+        // GET: DocumentView
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult PdfDocument()
+        {
+            string fileName = string.Empty;
+            var fileStream = new FileStream(
+                "~/Documentos/"+fileName,FileMode.Open,
+                FileAccess.Read);
+            var resultado = new FileStreamResult(fileStream,"application/pdf");
+            return resultado;
+        }
+
+    }
+}
